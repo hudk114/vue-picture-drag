c5fe6eb293160b6f44bde459832be83beea74f38
hudk <hdkwd@126.com>
2017-12-10 18:56:42 +0800

    add test

---
+++ test/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="app">
+        <picture-drag :img="img"
+            :title="'for my lover'"
+            :status="status"
+            :marks="marks">
+        </picture-drag>
+        <button type="button" name="button" @click="test">25</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                img: {
+                    src: require('./assets/WechatIMG136.png'),
+                    alt: 'hi',
+                    imgHeight: '1386',
+                    imgWidth: '1080',
+                },
+                status: [
+                    {
+                        name: 'boy',
+                        key: 0,
+                        color: 'rgb(148, 171, 193)',
+                    },
+                    {
+                        name: 'girl',
+                        key: 1,
+                        color: 'rgb(45, 209, 78)',
+                    },
+                ],
+                marks: [
+                    {
+                        name: 'hudk',
+                        status: 0,
+                        id: 0,
+                        left: 256,
+                        top: 251,
+                        index: 1, // if no index, would use default index in array
+                    },
+                    {
+                        name: 'kiki',
+                        status: 1,
+                        id: 1,
+                        left: 670,
+                        top: 131,
+                        index: 2,
+                    },
+                ],
+            };
+        },
+        methods: {
+            test() {
+                console.log(this.marks);
+            }        },
+    }
+</script>
+
+<style>
+</style>
---
+++ test/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import PictureDrag from 'vue-picture-drag';
+
+Vue.use(PictureDrag);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
